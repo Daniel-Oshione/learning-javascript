3386bd39495318db38267b5e0422b582e69f8a39
Daniel-Oshione <omobsdaniel@gmail.com>
2024-04-16 20:42:46 +0100

    added random number generator

--- test.js
+++ test.js
@@ -36,21 +36,58 @@
 
 
 // side task(create a counter program)
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
-const increase = document.getElementById("increase");
-const label = document.getElementById("update");
-let count = 0;
+// const decrease = document.getElementById("decrease");
+// const reset = document.getElementById("reset");
+// const increase = document.getElementById("increase");
+// const label = document.getElementById("update");
+// let count = 0;
 
-increase.onclick = function(){
-    count++;
-    label.textContent = count;
-}
-decrease.onclick = function(){
-    count--;
-    label.textContent = count;
-}
-reset.onclick = function(){
-    count = 0;
-    label.textContent = count;
+// increase.onclick = function(){
+//     count++;
+//     label.textContent = count;
+// }
+// decrease.onclick = function(){
+//     count--;
+//     label.textContent = count;
+// }
+// reset.onclick = function(){
+//     count = 0;
+//     label.textContent = count;
+// }
+
+
+
+//USING MATH 
+// let x = 100;
+// let y = 50;
+// let z = 3;
+// z = Math.round(x); to nearest whole
+// z = Math.ceil(x); to upper
+// z = Math.floor(x); to lower
+// z = Math.pow(x,y);
+// z = Math.sqrt(x,y); 
+// z = Math.log(x);  
+// let max = Math.max(x, y, z) to check for maximum variables 
+// let min = Math.min(x, y, z) to check for minimum variables
+// let rand = Math.floor(Math.random() * x) + y;
+// console.log(rand);
+
+// mini project (RANDOM NUMBER GENERATOR)
+const mybtn = document.getElementById("roll_btn");
+const rand1 = document.getElementById("random1");
+const rand2 = document.getElementById("random2");
+const rand3 = document.getElementById("random3");
+const min = 1;
+const max = 10;
+let randomnum1;
+let randomnum2;
+let randomnum3; 
+
+mybtn.onclick = function(){
+randomnum1 = Math.floor(Math.random() * max) + min;
+randomnum2 = Math.floor(Math.random() * max) + min;
+randomnum3 = Math.floor(Math.random() * max) + min;
+rand1.textContent = randomnum1;
+rand2.textContent = randomnum2;
+rand3.textContent = randomnum3;
 }
